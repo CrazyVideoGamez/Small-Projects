f65262960971c0da041457ba82a5ca606806ac38
Crazy Koder <48931026+CrazyVideoGamez@users.noreply.github.com>
2019-10-23 20:59:05 -0400

    Create factorize.py

---
+++ math_funcs/factorize.py
@@ -0,0 +1,33 @@
+from isPrime import isPrime
+
+def factorize(num:int):
+    if isPrime(num) == 'prime':
+        return str(num)
+    else:
+        divs = {}
+        while True:
+            try:
+                div = isPrime(num).split('not prime because of ')[1]
+            except IndexError:
+                full = ''
+                if num != 1:
+                    isthere = divs.get(num)
+                    if isthere:
+                        divs[num] += 1
+                    else:
+                        divs[num] = 1
+                for n,power in divs.items():
+                    full += str(n)
+                    if power != 1:
+                        full += '^' + str(power)
+                    full += ' x '
+                if full.endswith(' x '):
+                    full = full[:-3]
+                return full
+            isthere = divs.get(div)
+            if isthere:
+                divs[div] += 1
+            else:
+                divs[div] = 1
+            num /= int(div)
+            num = int(num)
