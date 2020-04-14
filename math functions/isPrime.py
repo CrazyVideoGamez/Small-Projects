df0332f4bf27ea90cf471b83a90e2ea85082c4b4
CrazyVideoGamez <59106025+CrazyVideoGamez@users.noreply.github.com>
2020-04-14 11:41:12 -0400

    Update isPrime.py

--- math functions/isPrime.py
+++ math functions/isPrime.py
@@ -2,8 +2,8 @@ def isPrime(num:int):
     if num <= 1:
         return 'undefined'
     else:
-        from math import floor
-        for i in range(2,floor(num/2)):
+        from math import ceil
+        for i in range(2,ceil(num/2)):
             if int(num/i) == num/i:
                 return 'not prime because of ' + str(i)
     return 'prime'
